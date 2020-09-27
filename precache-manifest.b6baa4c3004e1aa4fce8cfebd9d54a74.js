cb6e1985483253fdf3f8a0d03215504794e39dbe
artsashko <artyomsashko@gmail.com>
2020-09-28 00:51:56 +0300

    Updates

---
+++ precache-manifest.b6baa4c3004e1aa4fce8cfebd9d54a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09c0e40173f98fd03649a5c952bd072",
+    "url": "/tt-ossystem/index.html"
+  },
+  {
+    "revision": "8e66e3e178b5a10ade10",
+    "url": "/tt-ossystem/static/css/main.b219d215.chunk.css"
+  },
+  {
+    "revision": "4344a4cc774b65c5d87f",
+    "url": "/tt-ossystem/static/js/2.b0bcd62e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tt-ossystem/static/js/2.b0bcd62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e66e3e178b5a10ade10",
+    "url": "/tt-ossystem/static/js/main.5ef260cc.chunk.js"
+  },
+  {
+    "revision": "91e62464a26b526667a6",
+    "url": "/tt-ossystem/static/js/runtime-main.9f2fbb71.js"
+  }
+]);
