8207ea45bb891166a61bb7e8392f941c73866d58
artsashko <artyomsashko@gmail.com>
2020-09-29 17:44:28 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { clearProducts, 
